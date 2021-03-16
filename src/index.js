7a44bfd96f1cb5533b5ae2ddfa4ced5849a584e4
bang-a-coder <nikitas4717@gmail.com>
2021-03-16 14:27:27 +0100

    code cleanup

--- src/index.js
+++ src/index.js
@@ -14,66 +14,43 @@ let data = []
 createProjectBtn.addEventListener('click',() => {createProject()})
 
 function createProject(name){
-    viewID++
-    const newProjectSide = document.createElement('div')
-        newProjectSide.classList.add('project')
-        newProjectSide.innerHTML = name
-        newProjectSide.dataset.index = viewID
-        newProjectSide.addEventListener('click', ()=> {
-            visibleDiv.classList.add('displayN')
+  viewID++
 
-            newTask.taskito.classList.remove('displayN')
+  const newProjectSide = document.createElement('div')
+  newProjectSide.classList.add('project')
+  newProjectSide.innerHTML = name
+  newProjectSide.dataset.index = viewID
+  newProjectSide.addEventListener('click', ()=> {
+    visibleDiv.classList.add('displayN')
 
-            visibleDiv = newTask.taskito
-            
-        })
-    projectsDir.appendChild(newProjectSide)
-    
-    let newTask = new Task(mainDisplayArea, name, viewID)
-        if (name === 'HOME'){
-          visibleDiv = newTask.taskito
-        } else {
-          newTask.taskito.classList.add('displayN')
-        }
+    newTask.taskito.classList.remove('displayN')
 
-        newTask.taskito.setAttribute('style', 'border: none')
-        newTask.taskito.querySelector('.dltButton').remove()
-        newTask.taskito.querySelector('.save-button').remove()
-        newTask.taskito.querySelector('.due-date').remove()
-        newTask.taskito.querySelector('.textarea').remove()
-        eventFire(newTask.taskito, 'click')
+    visibleDiv = newTask.taskito
+      
+  })
+  
+  projectsDir.appendChild(newProjectSide)
+  
+  let newTask = new Task(mainDisplayArea, name, viewID)
+  if (name === 'HOME'){
+    visibleDiv = newTask.taskito
+  } else {
+    newTask.taskito.classList.add('displayN')
+  }
 
-        data.push(newTask)
-        console.log(data)
+  newTask.taskito.setAttribute('style', 'border: none')
+  newTask.taskito.querySelector('.dltButton').remove()
+  newTask.taskito.querySelector('.save-button').remove()
+  newTask.taskito.querySelector('.due-date').remove()
+  newTask.taskito.querySelector('.textarea').remove()
+  eventFire(newTask.taskito, 'click')
 
-    
-
-    //conntent.querySelector('#home-task').appendChild(newProjectInst)
-
-
-    
+  data.push(newTask)
+  console.log(data)
 }
-
 createProject('HOME')
 
-
-
-
-// let homeTask = new Task(mainDisplayArea, 'HOME', viewID)
-// console.log(homeTask.taskitoIndex)
-// homeTask.taskito.setAttribute('style', 'border: none')
-// homeTask.taskito.querySelector('.dltButton').remove()
-// homeTask.taskito.querySelector('.save-button').remove()
-// homeTask.taskito.querySelector('.due-date').remove()
-// homeTask.taskito.querySelector('.textarea').remove()
-// viewID++
-// data.push(homeTask)
-
-// console.log(data)
-
-// eventFire(homeTask.taskito, 'click')
-
-// visibleDiv = homeTask.taskito
+//HELPERS
 
 function eventFire(el, etype){
     if (el.fireEvent) {
@@ -84,23 +61,5 @@ function eventFire(el, etype){
       el.dispatchEvent(evObj);
     }
   }
-
-
-
-// let taskCreator = mainDisplayArea.querySelector('.create-task')
-//     let newTaskTitle = taskCreator.querySelector('.textarea-new')
-//     let createTaskBtn = taskCreator.querySelector(`#add-button`)
-
-
-// createTaskBtn.addEventListener('click', function() {
-//     console.log('creating')
-//     console.log(newTaskTitle.innerHTML)
-//     let newTask = new Task(mainDisplayArea, newTaskTitle.innerHTML, index)
-//     newTaskTitle.innerHTML = ''
-// })
-
-
-//let homeTask = new Task(conntent,'Oroborousss',index )
-
 
 console.log('working')
